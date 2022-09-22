658d37f908b29dab8672f32735cafb7348f1d4af
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-23 00:20:18 +0900

    Initial Commit

---
+++ BuildTinderApp/BuildTinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildTinderAppApp.swift
+//  BuildTinderApp
+//
+//  Created by 大久保樹 on 2022/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BuildTinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
