e072da407509f7df21ec104e63ebc37e84117781
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-28 21:46:24 +0900

    Swift進捗
    BuildTinderAppの作成

--- LikesView.swift
+++ LikesView.swift
@@ -16,14 +16,26 @@ struct LikesView: View {
     
     var body: some View {
         ScrollView(.vertical, showsIndicators: false, content: {
-            Text("Upgrade to Gold to see people who already liked you.")
-                .multilineTextAlignment(.center)
-                .lineLimit(2)
-                .foregroundColor(.textTitle)
-                .font(.system(size: 16))
-                .fontWeight(.medium)
-                .padding(.horizontal, 40)
+            if !user.goldSubscriber {
+                Text("Upgrade to Gold to see people who already liked you.")
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+                    .foregroundColor(.textTitle)
+                    .font(.system(size: 16))
+                    .fontWeight(.medium)
+                    .padding(.horizontal, 40)
                 .padding(.vertical, 24)
+            }
+            
+            LazyVGrid(
+                columns: [GridItem()],
+                alignment: .center,
+                spacing: nil,
+                pinnedViews: [],
+                content: {
+                    Text("Placeholder")
+                    Text("Placeholder")
+            })
         })
     }
 }
