66b4d9313d2cef2642dc3c090e3d6a85ec224c5a
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-30 23:43:34 +0900

    Swift進捗
    BuildTinderAppの作成

--- BuildTinderApp/MatchesView/LikesView.swift
+++ BuildTinderApp/MatchesView/LikesView.swift
@@ -7,59 +7,50 @@
 
 import SwiftUI
 
-//struct LikesView: View {
-//    @EnvironmentObject var userMng: UserManager
-//    @EnvironmentObject var appState: AppStateManager
-//
-//    private var user: User {
-//        return userMng.currentUser
-//    }
-    
-//    var body: some View {
-//        ScrollView(.vertical, showsIndicators: false, content: {
-//
-//            if !user.goldSubscriber {
-//                Text("Upgrade to Gold to see people who already liked you.")
-//                    .multilineTextAlignment(.center)
-//                    .lineLimit(2)
-//                    .foregroundColor(.textTitle)
-//                    .font(.system(size: 16, weight: .medium))
-//                    .padding(.horizontal, 40)
-//                    .padding(.vertical, 24)
-//            }
-//
-//            LazyVGrid(
-//                columns: [GridItem(.flexible()), GridItem(.flexible())],
-//                alignment: .center,
-//                spacing: nil,
-//                pinnedViews: [],
-//                content: {
-//                    ForEach(userMng.matches) { person in
-//                        PersonSquare(person: person, blur: !user.goldSubscriber)
-//                            .frame(height: 240)
-//                            .onTapGesture(perform: {
-//                                personTapped(person)
-//                            })
-//                    }
-//                })
-//                .padding(.horizontal, 6)
-//
-//        })
-//    }
-    
-//    func personTapped(_ person: Person) {
-//        if user.goldSubscriber {
-//            appState.showPersonsProfile(person)
-//        } else {
-//            appState.showPurchaseScreen()
-//        }
-//    }
-//}
+struct LikesView: View {
+    @EnvironmentObject var userMng: UserManager
+    @EnvironmentObject var appState: AppStateManager
 
-//struct LikesView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        LikesView()
-//            .environmentObject(UserManager())
-//            .environmentObject(AppStateManager())
-//    }
-//}
+    private var user: User {
+        return userMng.currentUser
+    }
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false, content: {
+
+            if !user.goldSubscriber {
+                Text("Upgrade to Gold to see people who already liked you.")
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+                    .foregroundColor(.textTitle)
+                    .font(.system(size: 16))
+                    .fontWeight(.medium)
+                    .padding(.horizontal, 40)
+                    .padding(.vertical, 24)
+            }
+
+            LazyVGrid(
+                columns: [GridItem(.flexible()), GridItem(.flexible())],
+                alignment: .center,
+                spacing: nil,
+                pinnedViews: [],
+                content: {
+                    Text("Placeholder")
+                    Text("Placeholder")
+                    Text("Placeholder")
+                    Text("Placeholder")
+                    Text("Placeholder")
+                    Text("Placeholder")
+                })
+                .padding(.horizontal, 6)
+
+        })
+    }
+}
+
+struct LikesView_Previews: PreviewProvider {
+    static var previews: some View {
+        LikesView()
+            .environmentObject(UserManager())
+    }
+}
