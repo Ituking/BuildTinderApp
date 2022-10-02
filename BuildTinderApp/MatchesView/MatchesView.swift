1889ab2d5314b589cefa21624fdb8e64f092279c
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-02 21:25:49 +0900

    Swift進捗
    BuildTinderAppの作成

--- BuildTinderApp/MatchesView/MatchesView.swift
+++ BuildTinderApp/MatchesView/MatchesView.swift
@@ -19,7 +19,7 @@ struct MatchesView: View {
     }
     
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             HStack {
                 Spacer()
                 
@@ -27,6 +27,9 @@ struct MatchesView: View {
                     Text("\(userMng.matches.count) Likes")
                         .font(.system(size: 22))
                         .fontWeight(.semibold)
+                        .if(selectedTab == .topPicks) {
+                            $0.foregroundColor(.textPrimary)
+                        }
                 })
                 .buttonStyle(PlainButtonStyle())
                 
@@ -36,10 +39,22 @@ struct MatchesView: View {
                     Text("\(userMng.topPicks.count) Top Picks")
                         .font(.system(size: 22))
                         .fontWeight(.semibold)
+                        .if(selectedTab == .likes) {
+                            $0.foregroundColor(.textPrimary)
+                        }
                 })
                 .buttonStyle(PlainButtonStyle())
                 
                 Spacer()
+            }
+            
+            Divider()
+                .padding(.vertical, 14)
+            
+            if selectedTab == .likes {
+                LikesView()
+            } else {
+                TopPicksView()
             }
             
             Spacer()
