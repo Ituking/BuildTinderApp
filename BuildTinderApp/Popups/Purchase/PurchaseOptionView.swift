47c29f27aefba8f18006422503f0a82083bcf6ef
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-03 23:57:49 +0900

    Swift進捗
    BuildTinderAppの作成

--- BuildTinderApp/Popups/Purchase/PurchaseOptionView.swift
+++ BuildTinderApp/Popups/Purchase/PurchaseOptionView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct PurchaseOptionView: View {
+    var sub: Subscription
+    var isSelected: Bool
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
@@ -15,6 +18,10 @@ struct PurchaseOptionView: View {
 
 struct PurchaseOptionView_Previews: PreviewProvider {
     static var previews: some View {
-        PurchaseOptionView()
+        let isSelected = true
+        
+        PurchaseOptionView(sub: Subscription.example1, isSelected: isSelected)
+        PurchaseOptionView(sub: Subscription.example2, isSelected: isSelected)
+        PurchaseOptionView(sub: Subscription.example3, isSelected: isSelected)
     }
 }
