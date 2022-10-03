7b9d97742f83584ceb5e2cd688a3e5c5a1513d9d
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-03 12:36:16 +0900

    Swift進捗
    BuildTinderAppの作成

---
+++ BuildTinderApp/Popups/PurchasePopup.swift
@@ -0,0 +1,30 @@
+//
+//  PurchasePopup.swift
+//  BuildTinderApp
+//
+//  Created by 大久保樹 on 2022/10/03.
+//
+
+import SwiftUI
+
+struct PurchasePopup: View {
+    var body: some View {
+        VStack {
+            Text("Get Tinder Gold")
+            
+            Text("PurchaseSwipePromo")
+            
+            Text("3 purchase Options")
+            
+            Text("Continue")
+            
+            Text("No Thanks")
+        }
+    }
+}
+
+struct PurchasePopup_Previews: PreviewProvider {
+    static var previews: some View {
+        PurchasePopup()
+    }
+}
