3438164ecb43800eda2d825e234e1bdcd9522ced
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-03 23:59:42 +0900

    Swift進捗
    BuildTinderAppの作成

--- BuildTinderApp/Popups/Purchase/PurchaseOptionView.swift
+++ BuildTinderApp/Popups/Purchase/PurchaseOptionView.swift
@@ -12,7 +12,9 @@ struct PurchaseOptionView: View {
     var isSelected: Bool
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            Text("\(sub.month)")
+        }
     }
 }
 
