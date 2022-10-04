bdfac27e72c3dcd2d73bdbcfc27012f2a07320c5
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-04 15:01:40 +0900

    Swift進捗
    BuildTinderAppの作成

--- BuildTinderApp/Popups/Purchase/PurchaseOptionView.swift
+++ BuildTinderApp/Popups/Purchase/PurchaseOptionView.swift
@@ -14,41 +14,35 @@ struct PurchaseOptionView: View {
     var body: some View {
         VStack(spacing: 0) {
             Text("\(sub.month)")
-                .font(.system(size: 44))
-                .fontWeight(.light)
+                .font(.system(size: 44, weight: .light))
             
-            Spacer()
-                .frame(height: 2)
+            Spacer().frame(height: 2)
             
             Text("month\(sub.month > 1 ? "s" : "")")
                 .if(isSelected) {
-                    $0.font(.system(size: 14))
-                        .fontWeight(.bold)
+                    $0.font(.system(size: 14, weight: .bold))
                 }
             
-            Spacer()
-                .frame(height: 2)
+            Spacer().frame(height: 2)
             
             Text("$\(String(format: "%.2f", sub.monthlyCost))/mo")
                 .foregroundColor(.textPrimary)
             
-            Spacer()
-                .frame(height: 2)
+            Spacer().frame(height: 10)
             
             if sub.savePercent != nil {
-                Text("\(sub.savePercent ?? 0)%")
+                Text("SAVE \(sub.savePercent ?? 0)%")
                     .foregroundColor(.yellow)
-                    .font(.system(size: 16))
-                    .fontWeight(.heavy)
+                    .font(.system(size: 16, weight: .heavy))
                     .frame(height: 22)
             } else {
-                Spacer()
-                    .frame(height: 22)
+                Spacer().frame(height: 22)
             }
             
-            Text("$\(String(format: "%.2f", sub.totalCost))/mo")
-                .font(.system(size: 20))
-                .fontWeight(.bold)
+            Spacer().frame(height: 12)
+            
+            Text("$\(String(format: "%.2f", sub.totalCost))")
+                .font(.system(size: 20, weight: .bold))
         }
         .padding(.horizontal, 12)
         .padding(.vertical, 18)
@@ -70,8 +64,7 @@ struct PurchaseOptionView: View {
                         .cornerRadius(10, corners: [.topLeft, .topRight])
                     
                     Text(sub.tagLine.rawValue)
-                        .font(.system(size: 12))
-                        .fontWeight(.bold)
+                        .font(.system(size: 12, weight: .bold))
                         .foregroundColor(.white)
                         .lineLimit(1)
                         .padding(.top, 2)
