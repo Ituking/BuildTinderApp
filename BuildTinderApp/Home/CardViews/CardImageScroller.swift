8fff83093fc40a25e210c86e5b26d1822e71280a
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-07 14:25:48 +0900

    Swift進捗
    BuildTinderAppの作成

---
+++ BuildTinderApp/Home/CardViews/CardImageScroller.swift
@@ -0,0 +1,24 @@
+//
+//  CardImageScroller.swift
+//  BuildTinderApp
+//
+//  Created by 大久保樹 on 2022/10/07.
+//
+
+import SwiftUI
+import KingfisherSwiftUI
+
+struct CardImageScroller: View {
+    
+    var person: Person
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardImageScroller_Previews: PreviewProvider {
+    static var previews: some View {
+        CardImageScroller(person: Person.example)
+    }
+}
