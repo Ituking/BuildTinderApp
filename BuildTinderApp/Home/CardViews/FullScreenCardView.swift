189f2c0d4d6548bc6d130220edafa516d4b5141a
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-08 12:05:05 +0900

    Swift進捗
    BuildTinderAppの作成

---
+++ BuildTinderApp/Home/CardViews/FullScreenCardView.swift
@@ -0,0 +1,33 @@
+//
+//  FullScreenCardView.swift
+//  BuildTinderApp
+//
+//  Created by 大久保樹 on 2022/10/08.
+//
+
+import SwiftUI
+
+struct FullScreenCardView: View {
+    var person: Person
+    
+    @Binding var fullScreenMode: Bool
+    
+    let screen = UIScreen.main.bounds
+    
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            VStack(spacing: 0) {
+                CardImageScroller(
+                    person: person,
+                    fullscreenMode: $fullScreenMode)
+                .frame(width: screen.width, height: screen.height * 0.6)
+            }
+        }
+    }
+}
+
+struct FullScreenCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FullScreenCardView(person: Person.example, fullScreenMode: .constant(true))
+    }
+}
